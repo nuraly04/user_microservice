b91684edd34521269f6d4e5ec112f5f6a348bff7
nuraly <nuralysaparaliev2@gmail.com>
2024-08-21 19:11:42 +0600

    injected querydsl

--- src/main/java/com/example/user_microservice/controller/skill/SkillController.java
+++ src/main/java/com/example/user_microservice/controller/skill/SkillController.java
@@ -2,13 +2,13 @@ package com.example.user_microservice.controller.skill;
 
 import com.example.user_microservice.dto.skill.SkillDto;
 import com.example.user_microservice.manager.skill.SkillManager;
-import com.example.user_microservice.service.skill.SkillService;
 import com.example.user_microservice.utils.Paths;
 import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,7 +30,7 @@ public class SkillController {
     }
 
     @GetMapping("/{userId}")
-    public List<SkillDto> getUserSkills(Long userId) {
+    public List<SkillDto> getUserSkills(@PathVariable Long userId) {
         return skillManager.getUserSkills(userId);
     }
 }
--- src/main/java/com/example/user_microservice/dto/skill/SkillDto.java
+++ src/main/java/com/example/user_microservice/dto/skill/SkillDto.java
@@ -12,6 +12,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class SkillDto {
 
+    private Long id;
+
     @NotBlank
     private String title;
 }
